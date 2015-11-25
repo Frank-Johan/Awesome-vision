9f6fc973bf91248a951df5628f0a3d9cb7ffe41a
tulevall <tulevall94@gmail.com>
2015-11-25 14:35:50 +0100

    Create main_awesome.php

---
+++ main_awesome.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Awesome vision</title>
+    
+     <link rel="icon" href="img/favicon.png">
+    <script src="jquery-2.1.1.js"></script>
+    <a href="file:///Applications/MAMP/htdocs/awesome/form.html"><img src="http://images.cooltext.com/4504378.png" width="618" height="73" alt="Awesome Vision" /></a>
+        
+<?php
+        //visar errormeddelanden
+        error_reporting(E_ALL);
+        ini_set('display_errors','ON');
+?>
+		
+        <?php
+        
+        
+        ?>
+        </style>
+            
+        </head>
+        <body>
+        <div id="edit">
+       <a href="adminform.php"><img src="edit.png"></a> 
+    
+   </div>
+  
+<style>
+#edit {
+    position: relative;
+    left: 700px;
+    
+    
+}
+    
+
+
+</style> 
+
+
+    </body>
+</html>
