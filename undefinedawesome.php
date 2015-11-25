cfecc3e1404b0303dae5af11621de1527dd7921f
thykinz <thykinz@gmail.com>
2015-11-25 14:40:11 +0100

    Update undefinedawesome.php

--- undefinedawesome.php
+++ undefinedawesome.php
@@ -1,26 +1,27 @@
-<!DOCTYPE html>
+
 <html>
 <head>
-  <title>Awesome Vision Undefined </title>
-    <link rel="stylesheet" type="text/css" href="XXXX">
+  <title>Awesome Vision Undefined</title>
+    <link rel="stylesheet" type="text/css">
     
 </head>
 <body>   
     <div class="undefinedbox">
-        <a href="form.php">
-        <img src="settingicon1.png" alt="settings" style="width:42px;height:42px;border:0">
+        <a href="adminform.php"> <img src="test.png"> </a>
+  
     
         </a>
-        <p><b>You have not yet defined what your Awesomeness is!</b></p>
-            <form action="demo_form.asp">
-                <input type="checkbox" name="awesome" value="awesome1"> Category1 <br>
-                <input type="checkbox" name="awesome" value="awesome2"> Category2 <br>
-                <input type="checkbox" name="awesome" value="awesome3"> Category3 <br>   
-                <input type="checkbox" name="awesome" value="awesome4"> Category4 <br>  
-            </form><br>
-        <p><b>To set your Awesome Vision click settings</b></p>
+       
     </div>
-<?php
-?>       
+    
+    <div>
+    To start your journey you must define your Awesome Vision. 
+        To define your awesome vision. Press <a href="adminform.php">here</a> or the X button up in the right corner
+    </div>
+     
 </body>
 </html>
+
+  
+
+
